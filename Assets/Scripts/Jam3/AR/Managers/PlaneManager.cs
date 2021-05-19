4c18affcaf0748d093a55ccfa7c5f75311a2f958
tostegroo <tostegroo@gmail.com>
2021-05-19 11:22:12 -0400

    update: scripts/settings

--- Assets/Scripts/Jam3/AR/Managers/PlaneManager.cs
+++ Assets/Scripts/Jam3/AR/Managers/PlaneManager.cs
@@ -19,10 +19,12 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using Unity.Collections;
 
 using Jam3.Util;
 
